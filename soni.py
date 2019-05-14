e86a357489a9b0308860d267051906f4ff02bd82
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-05-14 07:57:01 -0500

    Some changes.

--- soni.py
+++ soni.py
@@ -34,30 +34,46 @@ class soni():
 
         return err
 
-    def compare_images(self, imageA, imageB):
-        if imageA.shape != imageB.shape:
-            imageA_e, imageB_e = self.resize_imgs(imageA, imageB)
-        else:
-            imageA_e, imageB_e = imageA, imageB
+    def compare_images(self, imageA, imageB, kernel=30):
+        """Function to compare two images by swapping them."""
 
-        # compute the mean squared error and structural similarity
-        # index for the images
-        self.mse = self.mean_squared_error(imageA_e, imageB_e)
-        self.ssim = compare_ssim(imageA_e, imageB_e)
+        mse_v, ssim_v = [], []
+
+        for iB in range(imageB.shape[1] - kernel + 1):
+            for jB in range(imageB.shape[0] - kernel + 1):
+                for iA in range(imageA.shape[1] - kernel + 1):
+                    for jA in range(imageA.shape[0] - kernel + 1):
+                        mse_v.append(self.mean_squared_error(
+                            imageA[jA:jA + kernel, iA:iA + kernel],
+                            imageB[jB:jB + kernel, iB:iB + kernel]))
+                        ssim_v.append(compare_ssim(
+                            imageA[jA:jA + kernel, iA:iA + kernel],
+                            imageB[jB:jB + kernel, iB:iB + kernel]))
+
+        print(mse_v)
+        print(ssim_v)
+        print()
+
+        # # compute the mean squared error and structural similarity
+        # # index for the images
+        # self.mse = self.mean_squared_error(imageA_e, imageB_e)
+        # self.ssim = compare_ssim(imageA_e, imageB_e)
 
         # setup the figure
         fig = plt.figure("Image comparisson", figsize=(30, 20))
+        # plt.suptitle("MSE: %.2f, SSIM: %.2f" %
+        #              (self.mse, self.ssim), fontsize=60)
         plt.suptitle("MSE: %.2f, SSIM: %.2f" %
-                     (self.mse, self.ssim), fontsize=60)
+                     (np.mean(mse_v), np.mean(ssim_v)), fontsize=60)
 
         # show first image
         ax = fig.add_subplot(1, 2, 1)
-        plt.imshow(imageA_e, cmap=plt.cm.gray)
+        plt.imshow(imageA, cmap=plt.cm.gray)
         plt.axis("off")
 
         # show the second image
         ax = fig.add_subplot(1, 2, 2)
-        plt.imshow(imageB_e, cmap=plt.cm.gray)
+        plt.imshow(imageB, cmap=plt.cm.gray)
         plt.axis("off")
 
         # show the images
@@ -68,4 +84,4 @@ if __name__ == '__main__':
     img1 = rgb2gray(plt.imread('assets/img1.png'))
     img2 = rgb2gray(plt.imread('assets/img2.png'))
     soni = soni()
-    soni.compare_images(img1, img2)
+    soni.compare_images(img1, img2, kernel=img2.shape[1])
